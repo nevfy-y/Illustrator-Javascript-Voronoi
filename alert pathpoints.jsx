43de225fe2c1d22203f83b6d9433fb655df36484
fabiantheblind <fabiantheblind@googlemail.com>
2012-03-14 13:48:21 +0100

    util folder

--- alert pathpoints.jsx
+++
@@ -1,1 +0,0 @@
-﻿var count = 0;for (var i =0; i < app.activeDocument.selection.length;i++){try{count+=app.activeDocument.selection[i].pathPoints.length;}catch(e){}};alert("you have "+count+ " pathpoints in your selection");
